f2e19ceda86f61c106e3a81f2a52e29bb2619042
johannchopin <johannchopin@pm.me>
2022-03-05 12:57:40 +0100

    ✨ Init screen in main

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,20 +2,24 @@
 #include "pitches.h"
 #include "states.h"
 #include "button.h"
+#include "screen.h"
 
-const int buttonPin = 7; // the number of the pushbutton pin
+const int BUTTON_PIN = 7;
 
 States *states;
 Button *stateSwitchButton;
+Screen *screen;
 
 void setup()
 {
   Serial.begin(9600);
 
   states = new States();
-  stateSwitchButton = new Button(buttonPin);
+  stateSwitchButton = new Button(BUTTON_PIN);
+  screen = new Screen();
 
   stateSwitchButton->setup();
+  screen->setup();
 
   stateSwitchButton->onClick(std::bind(&States::goToNext, states));
 }
@@ -23,5 +27,5 @@ void setup()
 void loop()
 {
   stateSwitchButton->loop();
-  Serial.println(states->getCurrent());
+  screen->loop(const_cast<char*> (states->getCurrent()));
 }

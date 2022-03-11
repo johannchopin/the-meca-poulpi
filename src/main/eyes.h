192a14fcd0f5c59942beedba80fcd615bdcbc888
johannchopin <johannchopin@pm.me>
2022-03-11 17:22:47 +0100

    ✨ Meditation eye mode

--- src/main/eyes.h
+++ src/main/eyes.h
@@ -19,13 +19,22 @@ public:
   void loop(PoulpiState state);
   void setup();
   void setRgbColor(int r, int g, int b);
+  void setHslColor(float hue, float saturation, float lightness);
 
 private:
   ChainableLED *leds;
   void onStateChange();
   void handleSportAnimation();
+  void handleMeditationAnimation();
   int debounceTime = 0;
+
+  // sport animation
   int sportLightSwitchDelay = 100;
   Color *sportColors[SPORT_COLORS_AMOUNT] = {new Color(255, 0, 0), new Color(0, 255, 0), new Color(0, 0, 255)};
   int currentSportColorIndex = 0;
+  
+  // meditation animation
+  float meditationEyeOpacity = 0.5;
+  int meditationAnimationDelay = 15;
+  bool meditationEyeShouldFadeUp = true;
 };

c01a97a1c82577041a1c967328eda177f32335e8
Lyokolux <lyokolux@protonmail.com>
2022-03-12 14:19:13 +0100

    ✨(constants): better constants

--- src/main/constants.h
+++ src/main/constants.h
@@ -10,8 +10,24 @@ typedef enum {
   SLEEPY,
 } PoulpiState;
 
-const unsigned int DEFAULT_WATER_TO_DRINK_IN_ML = 2000;
+typedef enum {
+  TAKE_ON_ME,
+  BABY_ELEPHANT_WALK,
+  BRAHAMS_LULLABY,
+  CANTINABAND,
+  DOOM,
+  GREENSLEVES,
+  MII_CHANNEL_MUSIC
+} Music;
+
+typedef enum {
+  OFF = 0,
+  ON = 1,
+} Switch;
+
 const int DEFAULT_WATER_IN_GLASS_IN_ML = 200;
+const int DEFAULT_WATER_GOAL_IN_ML = 2000;
+
 const int MIN_WATER_LEVEL = 0;
 const int WATER_LEVEL_STEP_AMOUNT = 40;
-const int WATER_AMOUNT_IN_STEP_IN_ML = 25;
+const int WATER_AMOUNT_PER_STEP_IN_ML = 25;

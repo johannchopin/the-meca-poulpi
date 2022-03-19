f261301c3bbe6aed133987ab067da1d64db41669
johannchopin <johannchopin@pm.me>
2022-03-19 23:19:45 +0100

    ✨ Add mlValueStartColOnScreen util

--- src/main/localUtils.h
+++ src/main/localUtils.h
@@ -3,10 +3,15 @@
 #include "constants.h"
 #include "Arduino.h"
 
-class LocalUtils {
-  public:
-    static String* split(String phrase, String delimiter);
-    static int countItemsInArray(String* array);
+class LocalUtils
+{
+public:
+  static String *split(String phrase, String delimiter);
+  static int countItemsInArray(String *array);
+
+  // determine on which column the water amount should start
+  // to be printed on the screen
+  static int mlValueStartColOnScreen(int waterMl);
 };
 
 int availableMemory();

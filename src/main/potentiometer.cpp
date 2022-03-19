14d2654fe6a8588b2b9c3066ce516b2436852adf
johannchopin <johannchopin@pm.me>
2022-03-19 23:20:13 +0100

    ✨ Handle debounce in potentiometer

--- src/main/potentiometer.cpp
+++ src/main/potentiometer.cpp
@@ -1,17 +1,27 @@
 #include "potentiometer.h"
 
-void Potentiometer::setup() {
+void Potentiometer::setup()
+{
   pinMode(this->pin, OUTPUT);
 }
 
-void Potentiometer::loop(States* states) {
-  int value = analogRead(this->pin); // value between 0 and 1023
+void Potentiometer::loop(States *states)
+{
+  int current = millis();
+  bool isTimeToReadValue = (current - this->lastDebounceTime) > this->debounceDelay;
+  if (isTimeToReadValue)
+  {
+    int value = analogRead(this->pin); // value between 0 and 1023
 
-  // Reverse the potentiometer: low is high and high is low
-  // Map the value to different steps (25ml, 50ml, ...) to make it more confortable
-  int waterLevelSteps = WATER_LEVEL_STEP_AMOUNT - map(value, MIN_POTENTIOMETER_VALUE, MAX_POTENTIOMETER_VALUE, MIN_WATER_LEVEL, WATER_LEVEL_STEP_AMOUNT);
-  int waterGlassSizeInMlCandidate = waterLevelSteps * WATER_AMOUNT_PER_STEP_IN_ML;
-  if (states->waterGlassSizeInMl != waterGlassSizeInMlCandidate) {
-    states->waterGlassSizeInMl = waterGlassSizeInMlCandidate;
+    // Reverse the potentiometer: low is high and high is low
+    // Map the value to different steps (25ml, 50ml, ...) to make it more confortable
+    int waterLevelSteps = WATER_LEVEL_STEP_AMOUNT - map(value, MIN_POTENTIOMETER_VALUE, MAX_POTENTIOMETER_VALUE, MIN_WATER_LEVEL, WATER_LEVEL_STEP_AMOUNT);
+    int waterGlassSizeInMlCandidate = waterLevelSteps * WATER_AMOUNT_PER_STEP_IN_ML;
+    if (states->waterGlassSizeInMl != waterGlassSizeInMlCandidate)
+    {
+      states->waterGlassSizeInMl = waterGlassSizeInMlCandidate;
+    }
+
+    this->lastDebounceTime = current;
   }
 }
